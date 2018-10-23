3915319569e37e9e1ce791fdda967c128eb842fd
edsonha <edsonha@gmail.com>
2018-10-23 15:44:05 +0800

    Updates

---
+++ precache-manifest.735d3f9ed6a2248c83b2cb74b2a62d35.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "031dc66150e42d883a79a37682252e52",
+    "url": "/Star-Wars-Friends/static/media/Corellia.031dc661.png"
+  },
+  {
+    "revision": "606cf2ae4fa4ac418702",
+    "url": "/Star-Wars-Friends/static/css/main.153cb910.chunk.css"
+  },
+  {
+    "revision": "84ecfc55510163b8bbfe1aa9285123bf",
+    "url": "/Star-Wars-Friends/static/media/episode6.84ecfc55.jpg"
+  },
+  {
+    "revision": "49082a82bb5786926e92",
+    "url": "/Star-Wars-Friends/static/js/1.49082a82.chunk.js"
+  },
+  {
+    "revision": "64d8c5ebd1cfc766d18d",
+    "url": "/Star-Wars-Friends/static/js/runtime~main.64d8c5eb.js"
+  },
+  {
+    "revision": "3c138dee3381297709878164c6f16083",
+    "url": "/Star-Wars-Friends/static/media/Alderaan.3c138dee.png"
+  },
+  {
+    "revision": "d1e54b61fd9640843b88100f7e1f65ae",
+    "url": "/Star-Wars-Friends/static/media/Magnifier.d1e54b61.png"
+  },
+  {
+    "revision": "e73a83a483d6abda7733f9e856500650",
+    "url": "/Star-Wars-Friends/static/media/logo.e73a83a4.png"
+  },
+  {
+    "revision": "d9d33eb7ec33cf27bcd7a056a9e80d47",
+    "url": "/Star-Wars-Friends/static/media/gasgiant.d9d33eb7.jpg"
+  },
+  {
+    "revision": "ba871b85fd037be326659e668af605ff",
+    "url": "/Star-Wars-Friends/static/media/desert.ba871b85.jpg"
+  },
+  {
+    "revision": "d02d16065dc909fb9fbd3b270f79b673",
+    "url": "/Star-Wars-Friends/static/media/urban.d02d1606.jpg"
+  },
+  {
+    "revision": "e3153a8e08b42d34ab05ef5d677ffb07",
+    "url": "/Star-Wars-Friends/static/media/grasslands.e3153a8e.jpg"
+  },
+  {
+    "revision": "2eb97daf5b02d0f6a44443f5b62c1b3f",
+    "url": "/Star-Wars-Friends/static/media/grassyhills.2eb97daf.jpg"
+  },
+  {
+    "revision": "2a3a446e2cbea5b672f1c0d85378816d",
+    "url": "/Star-Wars-Friends/static/media/ocean.2a3a446e.jpg"
+  },
+  {
+    "revision": "b2e686d51bdda7f91d33313a122c579a",
+    "url": "/Star-Wars-Friends/static/media/islands.b2e686d5.jpg"
+  },
+  {
+    "revision": "e0b88900d008c08e8da77245d650b6b0",
+    "url": "/Star-Wars-Friends/static/media/laser.e0b88900.png"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/Star-Wars-Friends/static/media/stars.6494ab5b.png"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/Star-Wars-Friends/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "32d3a36ec7a6312c7c0ef45a1ad20da0",
+    "url": "/Star-Wars-Friends/static/media/Bespin.32d3a36e.png"
+  },
+  {
+    "revision": "51fc631af1e05c807081527b9cd4c68d",
+    "url": "/Star-Wars-Friends/static/media/Cato Neimoidia.51fc631a.png"
+  },
+  {
+    "revision": "606cf2ae4fa4ac418702",
+    "url": "/Star-Wars-Friends/static/js/main.606cf2ae.chunk.js"
+  },
+  {
+    "revision": "770334c2cc88b5f3c058e66f8c3807d8",
+    "url": "/Star-Wars-Friends/static/media/Coruscant.770334c2.png"
+  },
+  {
+    "revision": "4b90b85af46d6530bc9ae3b43d03b3fb",
+    "url": "/Star-Wars-Friends/static/media/Endor.4b90b85a.png"
+  },
+  {
+    "revision": "722ba92781281b096fff723515ed3ac3",
+    "url": "/Star-Wars-Friends/static/media/Geonosis.722ba927.png"
+  },
+  {
+    "revision": "3c1d13f9f9a167f7f7f7758d3e59a260",
+    "url": "/Star-Wars-Friends/static/media/Glee Anselm.3c1d13f9.png"
+  },
+  {
+    "revision": "a9a099e4c620bd5e47f6d65dee45b26f",
+    "url": "/Star-Wars-Friends/static/media/Iridonia.a9a099e4.png"
+  },
+  {
+    "revision": "44d28c593061b2e1b10fe70a2fa3e19c",
+    "url": "/Star-Wars-Friends/static/media/Kalee.44d28c59.png"
+  },
+  {
+    "revision": "e971bda82f2ebde37ebe4ce0bbb0037b",
+    "url": "/Star-Wars-Friends/static/media/Kamino.e971bda8.png"
+  },
+  {
+    "revision": "0636173ed4fce6af42e77ca4d7f43990",
+    "url": "/Star-Wars-Friends/static/media/Kashyyyk.0636173e.png"
+  },
+  {
+    "revision": "3bb0ef97c45b35fc4b844cc85b61240e",
+    "url": "/Star-Wars-Friends/static/media/MonCala.3bb0ef97.png"
+  },
+  {
+    "revision": "4174da17fb26d8d3074da8302242fde3",
+    "url": "/Star-Wars-Friends/static/media/Naboo.4174da17.png"
+  },
+  {
+    "revision": "6d587ccaab3b6318a107acda08734c68",
+    "url": "/Star-Wars-Friends/static/media/Nal Hutta.6d587cca.png"
+  },
+  {
+    "revision": "164726238a7ad6b0dd59935bc90fbb14",
+    "url": "/Star-Wars-Friends/static/media/Ryloth.16472623.png"
+  },
+  {
+    "revision": "912b11a6b92e6c9cefd23f77350251e8",
+    "url": "/Star-Wars-Friends/static/media/Tatooine.912b11a6.png"
+  },
+  {
+    "revision": "e48d91ad32675029970c56111095c653",
+    "url": "/Star-Wars-Friends/static/media/Umbara.e48d91ad.png"
+  },
+  {
+    "revision": "4f1dd7d3defbd31ae7d8d3e813b882f9",
+    "url": "/Star-Wars-Friends/static/media/episode1.4f1dd7d3.jpg"
+  },
+  {
+    "revision": "2475962906e510c4d62f7dbc39f26c43",
+    "url": "/Star-Wars-Friends/static/media/episode3.24759629.jpg"
+  },
+  {
+    "revision": "4b01fc64f6331c9444b9bf901ddf90fb",
+    "url": "/Star-Wars-Friends/static/media/episode4.4b01fc64.jpg"
+  },
+  {
+    "revision": "30ca2b2eea0046db2e4824d23bc9718b",
+    "url": "/Star-Wars-Friends/static/media/episode5.30ca2b2e.jpg"
+  },
+  {
+    "revision": "49082a82bb5786926e92",
+    "url": "/Star-Wars-Friends/static/css/1.1067a1b3.chunk.css"
+  },
+  {
+    "revision": "7e6456941323a042f17284f18bf0c7c3",
+    "url": "/Star-Wars-Friends/index.html"
+  }
+];
